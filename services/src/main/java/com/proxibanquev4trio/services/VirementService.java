f2d851dea3963364686a76be0be0b5a74aaba8ec
Guillaume Rio <guigzrio@gmail.com>
2016-10-10 02:31:18 +0200

    Begin repairing webservice integration

--- services/src/main/java/com/proxibanquev4trio/services/VirementService.java
+++ services/src/main/java/com/proxibanquev4trio/services/VirementService.java
@@ -1,12 +1,8 @@
 package com.proxibanquev4trio.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.proxibanquev4trio.domaine.Compte;
 import com.proxibanquev4trio.domaine.Virement;
 import com.proxibanquev4trio.logging.Loggable;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-import org.glassfish.jersey.client.ClientResponse;
 import org.springframework.stereotype.Component;
 
 /**
@@ -24,35 +20,35 @@ public class VirementService implements IVirementService {
     @Override
     @Loggable
     public void effectuerVirement(Virement virement) {
-        try {
-
-            Client client = Client.create();
-
-            WebResource webResource = client
-                    .resource("http://localhost:8080/proxibanquev4trio-webservices/virement/post");
-
-
-            //Transform virement into json parsed string
-            ObjectMapper mapper = new ObjectMapper();
-            String input = mapper.convertValue(virement, String.class);
-
-            ClientResponse response = webResource.type("application/json")
-                    .post(ClientResponse.class, input);
-
-            if (response.getStatus() != 201) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + response.getStatus());
-            }
-
-            System.out.println("Output from Server .... \n");
-            Virement output = response.readEntity(Virement.class);
-            System.out.println(output);
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
+//        try {
+//
+//            Client client = Client.create();
+//
+//            WebResource webResource = client
+//                    .resource("http://localhost:8080/proxibanquev4trio-webservices/virement/post");
+//
+//
+//            //Transform virement into json parsed string
+//            ObjectMapper mapper = new ObjectMapper();
+//            String input = mapper.convertValue(virement, String.class);
+//
+//            ClientResponse response = webResource.type("application/json")
+//                    .post(ClientResponse.class, input);
+//
+//            if (response.getStatus() != 201) {
+//                throw new RuntimeException("Failed : HTTP error code : "
+//                        + response.getStatus());
+//            }
+//
+//            System.out.println("Output from Server .... \n");
+//            Virement output = response.readEntity(Virement.class);
+//            System.out.println(output);
+//
+//        } catch (Exception e) {
+//
+//            e.printStackTrace();
+//
+//        }
     }
 
 
--- webservices/src/main/java/com/proxibanquev4trio/webservices/VirementWebService.java
+++ webservices/src/main/java/com/proxibanquev4trio/webservices/VirementWebService.java
@@ -1,14 +1,11 @@
 package com.proxibanquev4trio.webservices;
 
-import com.proxibanquev4trio.dao.IVirementDao;
 import com.proxibanquev4trio.domaine.Virement;
-import com.proxibanquev4trio.webservices.exception.VirementNotFeasibleException;
 
-import javax.annotation.security.RolesAllowed;
-import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 /**
  * Created by Guillaume on 09/10/2016.
@@ -16,40 +13,42 @@ import javax.ws.rs.core.Response;
 @Path("/virement")
 public class VirementWebService {
 
-    @Inject
-    IVirementDao virementDao;
+
+//    @Inject
+//    IVirementDao virementDao;
+
 
 
     @GET
     @Path("/get")
     @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed(value = {"conseiller", "gerant"})
-    public Virement afficherVirement() throws VirementNotFeasibleException {
+//    @RolesAllowed(value = {"conseiller", "gerant"})
+    public Virement afficherVirement() {
 
-            Virement virement = new Virement();
-            virement.setIdCrediteur(3L);
-            virement.setIdDebiteur(42L);
-            virement.setMontant(12345.6789);
+        Virement virement = new Virement();
+        virement.setIdCrediteur(3L);
+        virement.setIdDebiteur(42L);
+        virement.setMontant(12345.6789);
 
-            return virement;
-
-    }
-
-
-    @POST
-    @Path("/post")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @RolesAllowed(value = {"conseiller", "gerant"})
-    public Response createTrackInJSON(Virement virement) throws VirementNotFeasibleException {
-
-        try{
-
-            virementDao.save(virement);
-            return Response.status(201).entity(virement).build();
-
-        }catch (Exception e){
-            throw new VirementNotFeasibleException("Le virement n'a pu être effectué");
-        }
+        return virement;
 
     }
+
+
+//    @POST
+//    @Path("/post")
+//    @Consumes(MediaType.APPLICATION_JSON)
+////    @RolesAllowed(value = {"conseiller", "gerant"})
+//    public Response createTrackInJSON(Virement virement) throws VirementNotFeasibleException {
+//
+//        try{
+//
+//            virementDao.save(virement);
+//            return Response.status(201).entity(virement).build();
+//
+//        }catch (Exception e){
+//            throw new VirementNotFeasibleException("Le virement n'a pu être effectué");
+//        }
+//
+//    }
 }
