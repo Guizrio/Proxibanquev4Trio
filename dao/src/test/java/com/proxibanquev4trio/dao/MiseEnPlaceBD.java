0d05b185ed7f0b9cc16f361fa1401ca3d638820f
Fabrice LAVELLE <fabricelavelle1@hotmail.fr>
2016-10-11 14:07:00 +0200

    methode virement

--- dao/src/test/java/com/proxibanquev4trio/dao/MiseEnPlaceBD.java
+++ dao/src/test/java/com/proxibanquev4trio/dao/MiseEnPlaceBD.java
@@ -462,6 +462,8 @@ public class MiseEnPlaceBD {
 	    	gerant =new Gerant();
 	    	gerant.setNom("Vincent");
 	    	gerant.setRole("gerant");
+	    	gerant.setLogin("gerant");
+	    	gerant.setPwd("c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec");
 	    	List<Conseiller> liste2= new ArrayList<Conseiller>();
 	    	liste2.add(conseiller);
 	    	gerant.setConseillers(liste2);
--- services/src/main/java/com/proxibanquev4trio/services/VirementService.java
+++ services/src/main/java/com/proxibanquev4trio/services/VirementService.java
@@ -1,11 +1,13 @@
 package com.proxibanquev4trio.services;
 
+import com.proxibanquev4trio.dao.ICompteDao;
 import com.proxibanquev4trio.dao.IVirementDao;
 import com.proxibanquev4trio.domaine.Compte;
 import com.proxibanquev4trio.domaine.Virement;
 import com.proxibanquev4trio.logging.Loggable;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -22,7 +24,8 @@ public class VirementService implements IVirementService {
 	@Inject
 	IVirementDao virementDao;
 	
-	
+	@Inject
+	ICompteDao daoCompte;
 	
 	
 	public List<Virement> getAllVirement(){
@@ -31,7 +34,24 @@ public class VirementService implements IVirementService {
 		
 	}
 
-
+	public void virement(Compte compte1, Compte compte2, Double valeur){
+		
+		compte1.setSolde(compte1.getSolde()-valeur);
+		compte2.setSolde(compte2.getSolde()+valeur);
+		
+		daoCompte.save(compte1);
+		daoCompte.save(compte2);
+		
+		Virement virement = new Virement();
+		virement.setDate(new Date(System.currentTimeMillis()));
+		virement.setIdCrediteur(compte1.getId());
+		virement.setIdDebiteur(compte2.getId());
+		virement.setMontant(valeur);
+		
+		virementDao.save(virement);
+		
+		
+	}
 
     @Override
     @Loggable
@@ -91,6 +111,18 @@ public class VirementService implements IVirementService {
 
 	public void setVirementDao(IVirementDao virementDao) {
 		this.virementDao = virementDao;
+	}
+
+
+
+	public ICompteDao getDaoCompte() {
+		return daoCompte;
+	}
+
+
+
+	public void setDaoCompte(ICompteDao daoCompte) {
+		this.daoCompte = daoCompte;
 	}
 
 
